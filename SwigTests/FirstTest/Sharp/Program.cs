433521cb8f6c9731956a9e8c67289ec0055e6d13
Linda Schey <lindaschey@t-online.de>
2015-04-27 17:15:08 +0200

    todo

--- SwigTests/FirstTest/Sharp/Program.cs
+++ SwigTests/FirstTest/Sharp/Program.cs
@@ -15,8 +15,33 @@ namespace Sharp
             var myObject = new Object();
 
             var vec3 = new float3(1.1f, 2.2f, 3.3f);
-           
-            Console.Write(myObject.ray_cast(vec3, vec3, vec3, vec3, 2));
+            var bools = new bool[20];
+            for (int i = 0; i < 20; i++)
+            {
+                bools[i] = true;
+            }
+
+            
+            var floats = new float[24];
+            for (int i = 0; i < 24; i++)
+            {
+                floats[i] = 7.7f;
+            }
+
+            var ints = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                ints[i] =i;
+            }
+
+            myObject.mode(Object.mode_enum.mode_EDIT);
+            Console.WriteLine(myObject.layers());
+
+            string myString = "test";
+            myObject.active_material_index(5);
+            //Console.WriteLine(myObject.layers(bools));
+            int myInt = 5;
+            myObject.ray_cast(vec3, vec3, vec3, vec3, myInt);
             Console.Write("Done. Press any key to continue.");
             Console.ReadKey();
         }
